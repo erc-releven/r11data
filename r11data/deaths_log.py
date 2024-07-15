02a0e37944db926ab6d9d7bc4850c5bfcdacd040
Lukas Plank <lupl@tuta.io>
2024-07-15 15:16:21 +0200

    Deleted artefacts

--- r11data/deaths_log.py
+++
@@ -1,6 +0,0 @@
-from loguru import logger
-from r11data.utils.paths import logs
-
-logger.add(logs / "_deaths.log")
-
-logger.info("Logging from deaths_log")
--- r11data/starlegs_log.py
+++
@@ -1,6 +0,0 @@
-from loguru import logger
-from r11data.utils.paths import logs
-
-logger.add(logs / "starlegs.log")
-
-logger.info("Logging from starlegs_log")
