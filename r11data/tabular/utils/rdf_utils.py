dee6c5320e96efa97cb3ce31572bf7fbdbe9c23c
Lukas Plank <lupl@tuta.io>
2026-03-20 09:51:53 +0100

    feat: add ClosedOntologyNamespace functionality

--- r11data/tabular/utils/rdf_utils.py
+++ r11data/tabular/utils/rdf_utils.py
@@ -2,17 +2,142 @@ from collections.abc import Iterable, Iterator
 from hashlib import sha256
 import itertools
 import logging
+from pathlib import PurePath
+from types import MappingProxyType
 from typing import Literal as TypingLiteral, Self
+from typing import IO, TextIO
 from uuid import uuid4
 import warnings
 
-from lodkit import ClosedOntologyNamespace, NamespaceGraph, _Triple as Triple
+from lodkit import NamespaceGraph, _Triple as Triple
 from r11data.tabular.utils.date_parser import R11DateParser
 from r11data.utils.paths import ontologies_path
 from rdflib import Graph, Literal, Namespace, RDFS, URIRef
+from rdflib.parser import InputSource
+from rdflib.query import Result
 
 
 logger = logging.getLogger(__name__)
+
+
+type GraphParseSource = IO[bytes] | TextIO | InputSource | str | bytes | PurePath
+"""Source parameter type for rdflib.Graph.parse."""
+
+
+class NoSolutionException(Exception): ...
+
+
+class EmptySolutionException(Exception): ...  # pragma: no cover
+
+
+class ClosedOntologyNamespace:
+    """Ontology-based Namespace constructor.
+
+    ClosedOntologyNamespace allows constructing a namespace
+    based on an Ontology or generally an RDF graph source.
+
+    Given a lodkit.types.GraphParseSource or an rdflib.Graph,
+    the source is queried for RDF class and property definition assertions.
+    RDF term names are extracted by splitting the last IRI segment delimited by
+    '#', '/' or ':' and  matching name/IRI pairs are registered in the namespace mapping.
+
+    Namespace members are accessible as both attributes and items of a given
+    `ClosedOntologyNamespace` instance, i.e. attribute and item access is routed
+    to `ClosedOntologyNamespace.mapping`. For dictionary operations over the namespace mapping,
+    the public `ClosedOntologyNamespace.mapping` can be accessed directly.
+
+    In the case of RDF term names conflicting with class namespace names,
+    the class namespace names take precedence for attribute access;
+    conflicting RDF terms are still accessible via item lookup
+    or through the `ClosedOntologyNamespace.mapping` proxy.
+
+    """
+
+    _query = """
+    prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    prefix owl: <http://www.w3.org/2002/07/owl#>
+
+    select distinct ?name ?uri
+    where {
+    values ?type {
+      rdfs:Class
+      owl:Class
+
+      rdf:Property
+      owl:ObjectProperty
+      owl:DatatypeProperty
+      owl:AnnotationProperty
+
+      owl:NamedIndividual
+    }
+
+    ?uri a ?type .
+    filter (isIRI(?uri))
+
+    bind (replace(str(?uri), "^.*[#/:]", "") AS ?name)
+    filter (?name != "")
+    }
+    """
+
+    def __init__(self, source: GraphParseSource | Graph, *parse_args, **parse_kwargs):
+        self.source = source
+
+        graph: Graph = (
+            self.source
+            if isinstance(self.source, Graph)
+            else Graph().parse(source=self.source, *parse_args, **parse_kwargs)
+        )
+        sparql_result: Result = graph.query(self._query)
+
+        self.mapping: MappingProxyType[str, URIRef] = self._get_uris(
+            sparql_result=sparql_result
+        )
+
+    def __repr__(self) -> str:  # pragma: no cover
+        return f"<{self.__class__.__name__} source={self.source!r}>"
+
+    def __getattr__(self, value):
+        return self[value]
+
+    def __getitem__(self, key: str) -> URIRef:
+        try:
+            return self.mapping[key]
+        except KeyError:
+            raise AttributeError(
+                f"'{self.__class__.__name__}' object has no attribute '{key}'."
+            )
+
+    def _get_uris(self, sparql_result: Result) -> MappingProxyType[str, URIRef]:
+        _bindings = sparql_result.bindings
+
+        match _bindings:
+            case []:
+                raise NoSolutionException()
+            case [{**items}] if not items:  # pragma: no cover (unreachable)
+                raise EmptySolutionException()
+            case _:
+                return MappingProxyType(
+                    {
+                        str(binding["name"]): binding["uri"]  # type: ignore
+                        for binding in _bindings
+                    }
+                )
+
+
+crm = ClosedOntologyNamespace(source=ontologies_path / "crm.ttl")
+crmdig = ClosedOntologyNamespace(source=ontologies_path / "crmdig.ttl")
+lrm = ClosedOntologyNamespace(source=ontologies_path / "lrmoo.ttl")
+
+star = ClosedOntologyNamespace(source=ontologies_path / "star.ttl")
+r11spec = ClosedOntologyNamespace(source=ontologies_path / "r11spec.ttl")
+r11pros = ClosedOntologyNamespace(source=ontologies_path / "r11pros.ttl")
+pwro = ClosedOntologyNamespace(source=ontologies_path / "pwro.ttl")
+
+aaao = ClosedOntologyNamespace(source=ontologies_path / "aaao.ttl")
+so = ClosedOntologyNamespace(source=ontologies_path / "so.ttl")
+
+r11 = Namespace("https://r11.eu/rdf/resource/")
 
 
 class URIConstructorFactory:
@@ -36,19 +161,6 @@ class URIConstructorFactory:
         return self.namespace[segment]
 
 
-crm = ClosedOntologyNamespace(ontology=ontologies_path / "crm.ttl")
-crmdig = ClosedOntologyNamespace(ontology=ontologies_path / "crmdig.ttl")
-lrm = ClosedOntologyNamespace(ontology=ontologies_path / "lrmoo.ttl")
-
-r11 = Namespace("https://r11.eu/rdf/resource/")
-star = Namespace("https://r11.eu/ns/star/")
-r11spec = Namespace("https://r11.eu/ns/spec/")
-r11pros = Namespace("https://r11.eu/ns/prosopography/")
-pwro = Namespace("https://ontology.swissartresearch.net/pwro/")
-
-aaao = Namespace("https://ontology.swissartresearch.net/aaao/")
-so = Namespace("https://r11.eu/ns/similarity/")
-
 mkuri = URIConstructorFactory(r11)
 
 tara_uri = mkuri("0000-0001-6930-3470")
@@ -57,14 +169,7 @@ aleks_uri = mkuri("0009-0007-1432-0127")
 marton_uri = mkuri("0000-0003-3547-0750")
 
 
-class RelevenGraph(NamespaceGraph):
-    crmdig = crmdig
-    crm = crm
-
-    r11 = r11
-    star = star
-    r11spec = r11spec
-    r11pros = r11pros
+class RelevenGraph(NamespaceGraph): ...
 
 
 def get_source_name_lang_tag(
